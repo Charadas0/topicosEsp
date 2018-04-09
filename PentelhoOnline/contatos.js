d6cbfb419db7c3e8c7c490b1fbe3534fe82ee92f
RafaelFeiten <rafaelfeiten@sou.faccat.br>
2018-04-09 23:54:59 +0000

    js dos contatos

--- PentelhoOnline/contatos.js
+++ PentelhoOnline/contatos.js
@@ -1,10 +1,9 @@
-
 function aumenta(obj){
     obj.height=obj.height*2;
 	obj.width=obj.width*2;
 }
  
 function diminui(obj){
-	obj.height=obj.height/2;
-	obj.width=obj.width/2;
+	obj.height=obj.height;
+	obj.width=obj.width;
 }
