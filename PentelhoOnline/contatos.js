f851f3d2fea85a105b58436561c3db10b4e8b8ee
RafaelFeiten <rafaelfeiten@sou.faccat.br>
2018-04-10 00:10:25 +0000

    alteracao no contato.js

--- PentelhoOnline/contatos.js
+++ PentelhoOnline/contatos.js
@@ -1,14 +1,10 @@
 function aumenta(obj){
     (".loginlinks img").css("width", "60px" );
     (".loginlinks img").css("height", "60px" );
-    //obj.height=obj.height*2;
-	//obj.width=obj.width*2;
 }
  
 function diminui(obj){
 	(".loginlinks img").css("width", "40px" );
 	(".loginlinks img").css("height", "40px" );
 	
-	//obj.height=obj.height;
-	//obj.width=obj.width;
 }
