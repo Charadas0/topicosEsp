4327606e2c5e6336b92d6354d3ecdd0d430a5084
RafaelFeiten <rafaelfeiten@sou.faccat.br>
2018-04-10 23:05:07 +0000

    js title troca cor

--- PentelhoOnline/contatos.js
+++ PentelhoOnline/contatos.js
@@ -8,3 +8,8 @@ function diminui(obj){
 	(".loginlinks img").css("height", "40px" );
 	
 }
+
+function trocaCor(){
+   var cor = document.getElementById("corum").value;
+   document.getElementById("container").style.backgroundColor = cor;
+}
