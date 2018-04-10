8b59cf472d401ff0687fea400315b0707f5aa9d6
RafaelFeiten <rafaelfeiten@sou.faccat.br>
2018-04-10 23:53:53 +0000

    retirado o js contatos

--- PentelhoOnline/contatos.js
+++ PentelhoOnline/contatos.js
@@ -1,13 +1,3 @@
-function aumenta(obj){
-    (".loginlinks img").css("width", "60px" );
-    (".loginlinks img").css("height", "60px" );
-}
- 
-function diminui(obj){
-	(".loginlinks img").css("width", "40px" );
-	(".loginlinks img").css("height", "40px" );
-	
-}
 
 function trocaCor(){
    document.getElementById('corum').style.fontcolor = '#FFFFFF';
